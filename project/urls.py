5ef498e74d797745224f35587c22b29976e7ae2c
Pythondeveloper6 <pythondeveloper6@gmail.com>
2020-11-16 18:45:43 +0200

    --- Auto-Git Commit ---

--- project/urls.py
+++ project/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.urls import path , include
 from django.contrib import admin 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
@@ -25,3 +28,6 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
 
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
