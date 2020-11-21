ffc8fac8baf73ffbc1d1016aad865d4ace742324
Pythondeveloper6 <pythondeveloper6@gmail.com>
2020-11-21 20:52:44 +0200

    add translation v1

--- blog/models.py
+++ blog/models.py
@@ -4,18 +4,19 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
 from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
 class Post(models.Model):
-    author = models.ForeignKey(User,related_name='post_author' , on_delete=models.CASCADE)
-    title = models.CharField(max_length=50)
-    description = models.TextField(max_length=10000)
+    author = models.ForeignKey(User,related_name='post_author' , on_delete=models.CASCADE , verbose_name=_('author'))
+    title = models.CharField(max_length=50 , verbose_name=_('title'))
+    description = models.TextField(max_length=10000 , verbose_name=_('description'))
     created_at = models.DateTimeField(default=timezone.now)
-    image = models.ImageField(upload_to='blog/')
-    category = models.ForeignKey('Category', related_name='post_category', on_delete=models.CASCADE)
-    tags = TaggableManager(blank=True)
-    slug = models.SlugField(blank=True, null=True)
+    image = models.ImageField(_('image') ,upload_to='blog/')
+    category = models.ForeignKey('Category', related_name='post_category', on_delete=models.CASCADE , verbose_name=_('category'))
+    tags = TaggableManager(_('tags') ,blank=True)
+    slug = models.SlugField(_('slug') ,blank=True, null=True)
 
 
     def save(self, *args, **kwargs):
@@ -24,8 +25,8 @@ class Post(models.Model):
        super(Post, self).save(*args, **kwargs) # Call the real save() method
 
     class Meta:
-        verbose_name = ("Post")
-        verbose_name_plural = ("Posts")
+        verbose_name = _("Post")
+        verbose_name_plural = _("Posts")
 
     def __str__(self):
         return self.title
@@ -44,4 +45,5 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
 
--- project/settings.py
+++ project/settings.py
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ar'
 
 TIME_ZONE = 'UTC'
 
@@ -123,7 +123,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
